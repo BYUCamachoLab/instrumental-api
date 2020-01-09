c1d64a2edb07ba60332640b9dd940d0244fff35c
Sequoia Ploeg <sequoiap4@gmail.com>
2020-01-09 13:33:22 -0700

    Created a "time since" field

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -9,10 +9,29 @@ var app = new Vue({
     },
     methods: {
         async getMachines() {
-            console.log("index.js getItems()")
+            console.log("index.js getMachines()")
             try {
                 let response = await axios.get("/api/subservers");
                 this.machines = response.data;
+                for (let i = 0; i < this.machines.length; i++) {
+                    let then = moment(this.machines[i].updated);
+                    let now = moment();
+                    let ms = moment.duration(now.diff(then));
+                    let time_since = '';
+                    if (ms.days() > 28) {
+                        time_since = then.format("DD MMM YYYY")
+                    }
+                    else if (ms.days() > 0) {
+                        time_since = (ms.days() > 1) ? ms.days() + " days ago" : ms.days() + " day ago";
+                    } else if (ms.hours() > 0) {
+                        time_since = (ms.hours() > 1) ? ms.hours() + " hours ago" : ms.hours() + " hour ago";
+                    } else if (ms.minutes() > 0) {
+                        time_since = (ms.minutes() > 1) ? ms.minutes() + " minutes ago" : ms.minutes() + " minute ago";
+                    } else {
+                        time_since = ms.seconds() + " seconds ago";
+                    }
+                    this.machines[i].time_since = time_since;
+                }
                 return true;
             } catch (error) {
                 console.log(error);
@@ -23,7 +42,7 @@ var app = new Vue({
             try {
                 let response = await axios.get("/api/subservers/start/" + machine.id);
                 console.log(response.data);
-                // TODO: Refresh machine list on response
+                this.getMachines()
                 return true;
             } catch (error) {
                 console.log(error);
@@ -34,7 +53,7 @@ var app = new Vue({
             try {
                 let response = await axios.get("/api/subservers/stop/" + machine.id);
                 console.log(response.data);
-                // TODO: Refresh machine list on response
+                this.getMachines()
                 return true;
             } catch (error) {
                 console.log(error);
